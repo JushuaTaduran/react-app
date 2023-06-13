3a09cdc56d04951486b64ae1db2d08cb066b6bb3
JushuaTaduran <taduranjushua2@gmail.com>
2023-06-13 14:10:09 +0800

    worked with profile update

---
+++ src/components/Employee.js
@@ -0,0 +1,27 @@
+import EditEmployee from "./EditEmployee";
+
+function Employee(props) {
+  return (
+    <div className="min-w-[350px] max-w-[350px] m-2 py-8 px-8 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img
+        className="object-cover rouded-full h-[100px] w-[100px] block mx-auto rounded-full sm:mx-0 sm:shrink-0"
+        src={props.img}
+        alt="Profile"
+      />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">{props.name}</p>
+          <p className="text-slate-500 font-medium">{props.title}</p>
+        </div>
+        <EditEmployee
+          id={props.id}
+          name={props.name}
+          title={props.title}
+          updateEmployee={props.updateEmployee}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Employee;
